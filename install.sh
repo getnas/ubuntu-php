a67cbe41b8b23e92ea8c34165108e164c2faa6e0
Herald Yu <yuhr123@gmail.com>
2014-06-08 14:20:34 +0800

    add install script.

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#copy inti script to syttem server
+echo "Initialization..."
+cp ./php-fpm /etc/init.d/
+chmod +x /etc/init.d/php-fpm
+
+#Installation dependent libraries
+echo "Depends..."
+apt-get install -y libpcre3-dev zlib1g-dev libxml2-dev libssl-dev libbz2-dev libmcrypt-dev libpng-dev libjpeg-dev libfreetype6-dev libc-client-dev libtidy-dev libsnmp-dev
+
+#Set Environment Variables
+echo "Set Environment Variables..."
+cat pathinfo >> /etc/profile
+source /etc/profile
+
+#Set Startup Items
+echo "StartUp..."
+update-rc.d -f php-fpm defaults
+
+#Display PHP Version
+echo "The current version is:"
+php -v
